3ab574d6413e76b1ffa02caa1f35da2322bdad99
syu99 <ufcmjohan@gmail.com>
2023-04-07 16:01:34 +0200

    done

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/year', (req, res) => {
+ const year = new Date();
+ res.json({ now: year.getFullYear() });
+});
+
+module.exports = router;
